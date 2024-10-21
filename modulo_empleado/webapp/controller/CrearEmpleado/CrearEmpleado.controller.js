d41a8471f4105a7354e803bb0230b646ed29c67b
fjlork <fj.lorca.bocardo@gmail.com>
2024-10-21 22:06:00 +0000

    Mensaje

--- modulo_empleado/webapp/controller/CrearEmpleado/CrearEmpleado.controller.js
+++ modulo_empleado/webapp/controller/CrearEmpleado/CrearEmpleado.controller.js
@@ -1,216 +1,198 @@
 sap.ui.define([
-    'sap/ui/core/mvc/Controller', // Importa el controlador base de UI5
-    'sap/ui/model/json/JSONModel', // Importa el modelo JSON para la gestión de datos
-    "sap/m/MessageToast", // Importa el componente para mostrar mensajes emergentes
-    "sap/m/MessageBox" // Importa el componente para mostrar cuadros de diálogo
+	'sap/ui/core/mvc/Controller',
+	'sap/ui/model/json/JSONModel',
+	"sap/m/MessageToast",
+	"sap/m/MessageBox"
 ], function (Controller, JSONModel, MessageToast, MessageBox) {
-    "use strict";
+	"use strict";
 
-    // Definición del controlador específico "CrearEmpleado" en el módulo "lorkgroup.moduloempleado.controller.CrearEmpleado"
     return Controller.extend("lorkgroup.moduloempleado.controller.CrearEmpleado.CrearEmpleado", {
 
-        // Función de inicialización del controlador
-        onInit: function () {
-            // Asigna los elementos del wizard y contenedor de navegación a variables locales
-            this._wizard = this.byId("CreateProductWizard");
-            this._oNavContainer = this.byId("wizardNavContainer");
-            this._oWizardContentPage = this.byId("wizardContentPage");
+		onInit: function () {
+			this._wizard = this.byId("CreateProductWizard");
+			this._oNavContainer = this.byId("wizardNavContainer");
+			this._oWizardContentPage = this.byId("wizardContentPage");
 
-            // Creación del modelo de datos con valores iniciales
-            this.model = new JSONModel();
-            this.model.setData({
-                productNameState: "Error", // Estado inicial del nombre del producto
-                productWeightState: "Error" // Estado inicial del peso del producto
-            });
-            this.getView().setModel(this.model); // Establece el modelo en la vista actual
+			this.model = new JSONModel();
+			this.model.setData({
+				UserFirstNameState: "Error",
+				UserLastName: "Error"
+			});
+			this.getView().setModel(this.model);
+			this.model.setProperty("/UserType", "Mobile");
+			this.model.setProperty("/availabilityType", "In Store");
+			this.model.setProperty("/navApiEnabled", true);
+			this.model.setProperty("/productVAT", false);
+			this.model.setProperty("/measurement", "");
+			this._setEmptyValue("/productManufacturer");
+			this._setEmptyValue("/productDescription");
+			this._setEmptyValue("/size");
+			this._setEmptyValue("/productPrice");
+			this._setEmptyValue("/manufacturingDate");
+			this._setEmptyValue("/discountGroup");
 
-            // Propiedades adicionales del modelo que se inicializan
-            this.model.setProperty("/productType", "Mobile");
-            this.model.setProperty("/availabilityType", "In Store");
-            this.model.setProperty("/navApiEnabled", true);
-            this.model.setProperty("/productVAT", false);
-            this.model.setProperty("/measurement", "");
+		},
 
-            // Asigna valores vacíos a propiedades adicionales del modelo
-            this._setEmptyValue("/productManufacturer")
-            ;
-            this._setEmptyValue("/productDescription");
-            this._setEmptyValue("/size");
-            this._setEmptyValue("/productPrice");
-            this._setEmptyValue("/manufacturingDate");
-            this._setEmptyValue("/discountGroup");
-        },
+		setProductType: function (evt) {
+			var productType = evt.getSource().getTitle();
+			this.model.setProperty("/UserType", productType);
+			this.byId("ProductStepChosenType").setText("Chosen product type: " + productType);
+			this._wizard.validateStep(this.byId("ProductTypeStep"));
+		},
 
-        onNavigateToMenuPrincipal: function () {
-            var oRouter = sap.ui.core.UIComponent.getRouterFor(this);
-            oRouter.navTo("RouteApp");
-        },
+		setProductTypeFromSegmented: function (evt) {
+			var productType = evt.getParameters().item.getText();
+			this.model.setProperty("/UserType", productType);
+			this._wizard.validateStep(this.byId("ProductTypeStep"));
+		},
 
-        // Función que se ejecuta cuando se selecciona un tipo de producto
-        setProductType: function (evt) {
-            var productType = evt.getSource().getTitle(); // Obtiene el título del producto seleccionado
-            this.model.setProperty("/productType", productType); // Establece el tipo de producto en el modelo
-            this.byId("ProductStepChosenType").setText("Chosen product type: " + productType); // Actualiza el texto del paso con el tipo seleccionado
-            this._wizard.validateStep(this.byId("ProductTypeStep")); // Valida el paso del wizard correspondiente al tipo de producto
-        },
+		additionalInfoValidation: function () {
+			var name = this.byId("UserFirstName").getValue();
+			var weight = parseInt(this.byId("UserLastName").getValue());
 
-        // Función para manejar la selección del tipo de producto desde un control segmentado
-        setProductTypeFromSegmented: function (evt) {
-            var productType = evt.getParameters().item.getText(); // Obtiene el texto del elemento seleccionado
-            this.model.setProperty("/productType", productType); // Actualiza el tipo de producto en el modelo
-            this._wizard.validateStep(this.byId("ProductTypeStep")); // Valida el paso del wizard
-        },
+			if (isNaN(weight)) {
+				this._wizard.setCurrentStep(this.byId("ProductInfoStep"));
+				this.model.setProperty("/UserLastName", "Error");
+				this._wizard.invalidateStep(this.byId("ProductInfoStep"));
+			} else {
+				this.model.setProperty("/UserLastName", "None");
+				this._wizard.validateStep(this.byId("ProductInfoStep"));
+			}
 
-        // Validación de información adicional en los campos del formulario
-        additionalInfoValidation: function () {
-            var name = this.byId("ProductName").getValue(); // Obtiene el valor del campo "Nombre"
-            var weight = parseInt(this.byId("ProductWeight").getValue()); // Obtiene el valor del campo "Peso" y lo convierte a número entero
+			// if (name.length < 6) {
+			// 	this._wizard.setCurrentStep(this.byId("ProductInfoStep"));
+			// 	this.model.setProperty("/UserFirstName", "Error");
+			// } else {
+			// 	this.model.setProperty("/UserFirstName", "None");
+			// }
 
-            // Valida el campo "Peso" y actualiza su estado en el modelo
-            if (isNaN(weight)) {
-                this._wizard.setCurrentStep(this.byId("ProductInfoStep"));
-                this.model.setProperty("/productWeightState", "Error");
-            } else {
-                this.model.setProperty("/productWeightState", "None");
-            }
+			// if (name.length < 6 || isNaN(weight)) {
+			// 	this._wizard.invalidateStep(this.byId("ProductInfoStep"));
+			// } else {
+			// 	this._wizard.validateStep(this.byId("ProductInfoStep"));
+			// }
 
-            // Valida el campo "Nombre" y actualiza su estado en el modelo
-            if (name.length < 6) {
-                this._wizard.setCurrentStep(this.byId("ProductInfoStep"));
-                this.model.setProperty("/productNameState", "Error");
-            } else {
-                this.model.setProperty("/productNameState", "None");
-            }
 
-            // Si alguno de los campos no es válido, invalida el paso del wizard, de lo contrario lo valida
-            if (name.length < 6 || isNaN(weight)) {
-                this._wizard.invalidateStep(this.byId("ProductInfoStep"));
-            } else {
-                this._wizard.validateStep(this.byId("ProductInfoStep"));
-            }
-        },
 
-        // Muestra un mensaje emergente cuando se activa el paso opcional del wizard
-        optionalStepActivation: function () {
-            MessageToast.show('This event is fired on activate of Step3.');
-        },
 
-        // Muestra un mensaje emergente cuando se completa el paso opcional del wizard
-        optionalStepCompletion: function () {
-            MessageToast.show('This event is fired on complete of Step3. You can use it to gather the information, and lock the input data.');
-        },
+		},
 
-        // Habilita la navegación del wizard en la sección de precios
-        pricingActivate: function () {
-            this.model.setProperty("/navApiEnabled", true);
-        },
+		optionalStepActivation: function () {
+			MessageToast.show(
+				'This event is fired on activate of Step3.'
+			);
+		},
 
-        // Deshabilita la navegación del wizard en la sección de precios
-        pricingComplete: function () {
-            this.model.setProperty("/navApiEnabled", false);
-        },
+		optionalStepCompletion: function () {
+			MessageToast.show(
+				'This event is fired on complete of Step3. You can use it to gather the information, and lock the input data.'
+			);
+		},
 
-        // Navega desde el paso 4 al paso 2 del wizard
-        scrollFrom4to2: function () {
-            this._wizard.goToStep(this.byId("ProductInfoStep"));
-        },
+		pricingActivate: function () {
+			this.model.setProperty("/navApiEnabled", true);
+		},
 
-        // Navega desde el paso 4 al paso 3 del wizard
-        goFrom4to3: function () {
-            if (this._wizard.getProgressStep() === this.byId("PricingStep")) {
-                this._wizard.previousStep();
-            }
-        },
+		pricingComplete: function () {
+			this.model.setProperty("/navApiEnabled", false);
+		},
 
-        // Navega desde el paso 4 al paso 5 del wizard
-        goFrom4to5: function () {
-            if (this._wizard.getProgressStep() === this.byId("PricingStep")) {
-                this._wizard.nextStep();
-            }
-        },
+		scrollFrom4to2: function () {
+			this._wizard.goToStep(this.byId("ProductInfoStep"));
+		},
 
-        // Maneja la acción cuando el wizard se completa
-        wizardCompletedHandler: function () {
-            this._oNavContainer.to(this.byId("wizardReviewPage"));
-        },
+		goFrom4to3: function () {
+			if (this._wizard.getProgressStep() === this.byId("PricingStep")) {
+				this._wizard.previousStep();
+			}
+		},
 
-        // Retorna al contenido principal del wizard
-        backToWizardContent: function () {
-            this._oNavContainer.backToPage(this._oWizardContentPage.getId());
-        },
+		goFrom4to5: function () {
+			if (this._wizard.getProgressStep() === this.byId("PricingStep")) {
+				this._wizard.nextStep();
+			}
+		},
 
-        // Métodos para editar pasos específicos del wizard
-        editStepOne: function () { this._handleNavigationToStep(0); },
-        editStepTwo: function () { this._handleNavigationToStep(1); },
-        editStepThree: function () { this._handleNavigationToStep(2); },
-        editStepFour: function () { this._handleNavigationToStep(3); },
+		wizardCompletedHandler: function () {
+			this._oNavContainer.to(this.byId("wizardReviewPage"));
+		},
 
-        // Navega a un paso específico del wizard
-        _handleNavigationToStep: function (iStepNumber) {
-            var fnAfterNavigate = function () {
-                this._wizard.goToStep(this._wizard.getSteps()[iStepNumber]); // Navega al paso especificado
-                this._oNavContainer.detachAfterNavigate(fnAfterNavigate); // Desvincula la navegación posterior
-            }.bind(this);
+		backToWizardContent: function () {
+			this._oNavContainer.backToPage(this._oWizardContentPage.getId());
+		},
 
-            this._oNavContainer.attachAfterNavigate(fnAfterNavigate);
-            this.backToWizardContent();
-        },
+		editStepOne: function () {
+			this._handleNavigationToStep(0);
+		},
 
-        // Muestra un cuadro de diálogo con el mensaje y tipo especificado
-        _handleMessageBoxOpen: function (sMessage, sMessageBoxType) {
-            MessageBox[sMessageBoxType](sMessage, {
-                actions: [MessageBox.Action.YES, MessageBox.Action.NO],
-                onClose: function (oAction) {
-                    if (oAction === MessageBox.Action.YES) {
-                        this._handleNavigationToStep(0);
-                        this._wizard.discardProgress(this._wizard.getSteps()[0]);
-                    }
-                }.bind(this)
-            });
-        },
+		editStepTwo: function () {
+			this._handleNavigationToStep(1);
+		},
 
-        // Asigna un valor vacío a la propiedad especificada en el modelo
-        _setEmptyValue: function (sPath) {
-            this.model.setProperty(sPath, "n/a");
-        },
+		editStepThree: function () {
+			this._handleNavigationToStep(2);
+		},
 
-        // Maneja la cancelación del wizard
-        handleWizardCancel: function () {
-            this._handleMessageBoxOpen("Are you sure you want to cancel your report?", "warning");
-        },
+		editStepFour: function () {
+			this._handleNavigationToStep(3);
+		},
 
-        // Maneja la confirmación del wizard
-        handleWizardSubmit: function () {
-            this._handleMessageBoxOpen("Are you sure you want to submit your report?", "confirm");
-        },
+		_handleNavigationToStep: function (iStepNumber) {
+			var fnAfterNavigate = function () {
+				this._wizard.goToStep(this._wizard.getSteps()[iStepNumber]);
+				this._oNavContainer.detachAfterNavigate(fnAfterNavigate);
+			}.bind(this);
 
-        // Formateador para el estado del peso del producto
-        productWeighStateFormatter: function (val) {
-            return isNaN(val) ? "Error" : "None"; // Retorna "Error" si el valor no es un número, de lo contrario retorna "None"
-        },
+			this._oNavContainer.attachAfterNavigate(fnAfterNavigate);
+			this.backToWizardContent();
+		},
 
-        // Descartar el progreso del wizard y limpiar los datos del formulario
-        discardProgress: function () {
-            this._wizard.discardProgress(this.byId("ProductTypeStep")); // Descartar el progreso del paso inicial
+		_handleMessageBoxOpen: function (sMessage, sMessageBoxType) {
+			MessageBox[sMessageBoxType](sMessage, {
+				actions: [MessageBox.Action.YES, MessageBox.Action.NO],
+				onClose: function (oAction) {
+					if (oAction === MessageBox.Action.YES) {
+						this._handleNavigationToStep(0);
+						this._wizard.discardProgress(this._wizard.getSteps()[0]);
+					}
+				}.bind(this)
+			});
+		},
 
-            var clearContent = function (content) {
-                for (var i = 0; i < content.length; i++) {
-                    if (content[i].setValue) {
-                        content[i].setValue(""); // Limpia el valor del contenido si existe
-                    }
+		_setEmptyValue: function (sPath) {
+			this.model.setProperty(sPath, "n/a");
+		},
 
-                    if (content[i].getContent) {
-                        clearContent(content[i].getContent()); // Llamada recursiva para limpiar contenido anidado
-                    }
-                }
-            };
+		handleWizardCancel: function () {
+			this._handleMessageBoxOpen("Are you sure you want to cancel your report?", "warning");
+		},
 
-            // Restablece los estados del modelo a "Error"
-            this.model.setProperty("/productWeightState", "Error");
-            this.model.setProperty("/productNameState", "Error");
+		handleWizardSubmit: function () {
+			this._handleMessageBoxOpen("Are you sure you want to submit your report?", "confirm");
+		},
 
-            // Limpia todo el contenido de los pasos del wizard
-            clearContent(this._wizard.getSteps());
-        }
-    });
+		productWeighStateFormatter: function (val) {
+			return isNaN(val) ? "Error" : "None";
+		},
+
+		discardProgress: function () {
+			this._wizard.discardProgress(this.byId("ProductTypeStep"));
+
+			var clearContent = function (content) {
+				for (var i = 0; i < content.length; i++) {
+					if (content[i].setValue) {
+						content[i].setValue("");
+					}
+
+					if (content[i].getContent) {
+						clearContent(content[i].getContent());
+					}
+				}
+			};
+
+			this.model.setProperty("/UserLastName", "Error");
+			this.model.setProperty("/UserFirstName", "Error");
+			clearContent(this._wizard.getSteps());
+		}
+	});
 });
--- modulo_empleado/webapp/controller/CrearEmpleado2/CrearEmpleado2.controller.js
+++ modulo_empleado/webapp/controller/CrearEmpleado2/CrearEmpleado2.controller.js
@@ -1,103 +1,80 @@
 sap.ui.define([
-    'sap/ui/core/mvc/Controller',
-    'sap/ui/model/json/JSONModel',
-    "sap/m/MessageToast",
-    "sap/m/MessageBox"
-], function (Controller, JSONModel, MessageToast, MessageBox) {
+    "sap/ui/core/mvc/Controller",
+    "sap/ui/model/json/JSONModel",
+    "sap/m/MessageToast"
+], function(Controller, JSONModel, MessageToast) {
     "use strict";
 
     return Controller.extend("lorkgroup.moduloempleado.controller.CrearEmpleado2.CrearEmpleado2", {
-        
-                onInit: function () {
-                    // Inicializa un modelo JSON vacío para la vista
-                    var oModel = new JSONModel();
-                    this.getView().setModel(oModel, "userData");
-                },
 
-                onNavigateToMenuPrincipal: function () {
-                    var oRouter = sap.ui.core.UIComponent.getRouterFor(this);
-                    oRouter.navTo("RouteApp");
+        onInit: function() {
+            // Cargar el modelo OData
+            var oDataModel = new sap.ui.model.odata.v2.ODataModel("/sap/opu/odata/sap/ZEMPLOYEES_SRV/");
+            this.getView().setModel(oDataModel);
+
+            // Crear un modelo JSON para datos locales de formulario
+            var oLocalModel = new JSONModel({
+                User: {
+                    EmployeeId: "",
+                    SapId: "",
+                    FirstName: "",
+                    LastName: "",
+                    Dni: "",
+                    CreationDate: new Date(),
+                    Comments: ""
                 },
-        
-                onSave: function () {
-                    // Obtiene el modelo del servicio OData
-                    var oModel = this.getView().getModel();
-        
-                    // 1. Crear el nuevo usuario
-                    var oNewUser = {
-                        // UserID: this.getView().byId("userId").getValue(),
-                        UserID: parseInt(this.getView().byId("userId").getValue(), 10),  // Convierte a número entero
-                        FirstName: this.getView().byId("userName").getValue(),
-                        LastName: this.getView().byId("userLastName").getValue(),
-                        Email: this.getView().byId("userEmail").getValue(),
-                        Phone: this.getView().byId("userPhone").getValue()
-                    };
-        
-                    // Valida que los campos obligatorios no estén vacíos
-                    if (!oNewUser.UserID || !oNewUser.FirstName || !oNewUser.LastName || !oNewUser.Email) {
-                        MessageBox.error("Por favor, complete todos los campos obligatorios.");
-                        return;
-                    }
-        
-                    // Crear el usuario en el servicio OData
-                    oModel.create("/Users", oNewUser, {
-                        success: function (oUserData) {
-                            MessageToast.show("Usuario creado con éxito");
-        
-                            // 2. Crear el salario asociado al usuario
-                            var oNewSalary = {
-                                UserID: oUserData.UserID, // Usar el ID del usuario creado
-                                BaseSalary: parseFloat(this.getView().byId("baseSalary").getValue()),
-                                Bonus: parseFloat(this.getView().byId("bonus").getValue())
-                            };
-        
-                            // Crear el salario en el servicio OData
-                            oModel.create("/Salaries", oNewSalary, {
-                                success: function () {
-                                    MessageToast.show("Salario asociado al usuario creado con éxito");
-        
-                                    // 3. Subir el archivo adjunto (si existe)
-                                    var oFileUploader = this.getView().byId("fileUploader");
-                                    var oFile = oFileUploader.getFocusDomRef().files[0];
-        
-                                    if (oFile) {
-                                        var oAttachment = {
-                                            UserID: oUserData.UserID // Usar el ID del usuario creado
-                                        };
-        
-                                        var sPath = "/Attachments";
-                                        var sUrl = oModel.sServiceUrl + sPath;
-                                        var oHeaders = {
-                                            "Content-Type": oFile.type,
-                                            "slug": oFile.name
-                                        };
-        
-                                        // Realizar una llamada AJAX para subir el archivo
-                                        $.ajax({
-                                            url: sUrl,
-                                            type: "POST",
-                                            headers: oHeaders,
-                                            processData: false,
-                                            contentType: false,
-                                            data: oFile,
-                                            success: function () {
-                                                MessageToast.show("Documento adjunto subido con éxito");
-                                            },
-                                            error: function (oError) {
-                                                MessageBox.error("Error al subir el archivo adjunto: " + oError.message);
-                                            }
-                                        });
-                                    }
-                                }.bind(this),
-                                error: function (oError) {
-                                    MessageBox.error("Error al crear el salario asociado: " + oError.message);
-                                }
-                            });
-                        }.bind(this),
-                        error: function (oError) {
-                            MessageBox.error("Error al crear el usuario: " + oError.message);
-                        }
-                    });
+                Salary: {
+                    SalaryId: "",
+                    SapId: "",
+                    Amount: "",
+                    Waers: "",
+                    CreationDate: new Date(),
+                    Comments: ""
                 }
             });
-        });
+            this.getView().setModel(oLocalModel, "local");
+        },
+
+        onSave: function() {
+            // Obtener el modelo OData y el modelo local
+            var oDataModel = this.getView().getModel();  // OData model
+            var oLocalModel = this.getView().getModel("local");  // JSON local model
+
+            // Obtener los datos del formulario (desde el modelo local)
+            var oUserData = oLocalModel.getProperty("/User");
+            var oSalaryData = oLocalModel.getProperty("/Salary");
+
+            // Validación simple de datos antes de enviar (puedes agregar más validaciones si es necesario)
+            if (!oUserData.EmployeeId || !oUserData.FirstName || !oUserData.LastName) {
+                MessageToast.show("Por favor, complete todos los campos obligatorios del empleado.");
+                return;
+            }
+            if (!oSalaryData.SalaryId || !oSalaryData.Amount) {
+                MessageToast.show("Por favor, complete todos los campos obligatorios del salario.");
+                return;
+            }
+
+            // Crear la entidad User en OData
+            oDataModel.create("/Users", oUserData, {
+                success: function() {
+                    MessageToast.show("Empleado creado exitosamente.");
+                },
+                error: function(oError) {
+                    MessageToast.show("Error al crear el empleado.");
+                    console.error(oError);  // Para depurar el error en caso de que ocurra
+                }
+            });
+
+            // Crear la entidad Salary en OData
+            oDataModel.create("/Salaries", oSalaryData, {
+                success: function() {
+                    MessageToast.show("Salario creado exitosamente.");
+                },
+                error: function(oError) {
+                    MessageToast.show("Error al crear el salario.");
+                    console.error(oError);  // Para depurar el error en caso de que ocurra
+                }
+            });
+        }
+    });
+});
